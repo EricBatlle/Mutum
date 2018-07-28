c0c79a29e4bd983afd45bb4aefd6ec528deca112
Eric <erikbc_95@hotmail.com>
2018-07-28 16:51:51 +0200

    Initial commit

---
+++ Assets/GoogleMaps/MapScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapScript : MonoBehaviour {
+
+    string url = "";
+
+    public float lat = 41.3911056f;
+    public float lon = 2.1548483f;
+    LocationInfo li;
+    public int zoom = 14;
+    public int mapWidth = 640;
+    public int mapHeight = 640;
+    public enum mapType { roadmap, satellite, hybrid, terrain };
+    public mapType mapSelected;
+    public int scale;
+
+    private bool loadingMap = false;
+
+    private IEnumerator mapCoroutine;
+
+    private void Start()
+    {
+        mapCoroutine = GetGoogleMap(lat, lon);
+        StartCoroutine(mapCoroutine);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            print("new Map");
+            mapCoroutine = GetGoogleMap(lat, lon);
+            StartCoroutine(mapCoroutine);
+        }
+    }
+
+    IEnumerator GetGoogleMap(float lat, float lon)
+    {
+        url = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat +","+ lon +
+            "&zoom="+ zoom +"&size="+ mapWidth + "x"+ mapHeight +"&scale="+ scale +
+            "&maptype="+ mapSelected +
+            "&key=AIzaSyBd8uWGVeXThrT2SjbnMrAhQDiCp19gmlw";
+        print(url);
+        loadingMap = true;
+        WWW www = new WWW(url);
+        print(www.error);
+        yield return www;
+        loadingMap = false;
+        //Assign downloaded map texture to Canvas Image
+        gameObject.GetComponent<RawImage>().texture = www.texture;
+        StopCoroutine(mapCoroutine);
+    }
+
+}
---
+++ Assets/UserLocation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserLocation : MonoBehaviour {
+
+    [SerializeField] private Text text;
+
+	// Use this for initialization
+	void Start () {
+        //Turn on location Services
+        Input.location.Start(10.0f,10.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateLocation();
+	}
+
+    private void UpdateLocation()
+    {
+        //Do nothing if location services are not available
+        if(Input.location.isEnabledByUser)
+        {
+            float lat = Input.location.lastData.latitude;
+            float lon = Input.location.lastData.longitude;
+
+            text.text = "Lattitude:" + lat + "Longitude:" + lon;
+        }
+    }
+}
