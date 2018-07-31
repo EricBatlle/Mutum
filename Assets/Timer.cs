ee0712e4f1495854419de31c8deffec251cdb5e0
Eric <erikbc_95@hotmail.com>
2018-08-01 00:18:23 +0200

    first attempt with google play store purchase, waiting feedback

---
+++ Assets/Timer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    private float time = 0.0f;
+
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        print(time);
+	}
+}
